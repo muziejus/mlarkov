2913eabb562c846df7abcf7bb5543be5cd69d6cd
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-04-13 18:46:28 +0300

    Get running the bot code out of the class definition.

---
+++ run_bot.rb
@@ -0,0 +1,25 @@
+require_relative 'KarlMarxovChain'
+
+tweet = KarlMarxovChain.new
+if ARGV[0] == "random" # Called from the command line to make a random sentence.
+  tweet_text = tweet.random_sentence
+  # tweet.tweet(tweet_text)
+  puts tweet_text
+elsif ARGV[0] == "term" # Called from the command line to seed a sentence.
+  tweet.triple_array
+  tweet.tweet(tweet.build_sentence(ARGV[1]))
+else # must a cron job, looking for @s.
+  tweet.triple_array
+  reply_ids = []
+  tweet.replies.each do |reply|
+    reply_ids.push(reply.id)
+    # puts "reply: #{reply.id} - #{reply.text}"
+    match = /^.*@KarlMarxovChain (\w*).*$/i.match(reply.text)
+    match.nil? ? term = "No term detected" : term = match[1]
+    tweet_text = tweet.build_sentence(term)
+    tweet_text = "@" + reply.user.screen_name + " " + tweet_text
+    tweet.tweet(tweet_text, reply.id)
+  end
+  tweet.update_since_id(reply_ids.max) unless reply_ids.max.nil?
+end
+
