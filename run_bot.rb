f14c04e59501508180ae16552736efea79f20251
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-12-17 23:46:20 -0500

    Mess around with @ing.

--- run_bot.rb
+++ run_bot.rb
@@ -8,15 +8,12 @@ elsif ARGV[0] == "term" # Called from the command line to seed a sentence.
   tweet.set_triple_array
   tweet.tweet(tweet.build_sentence(ARGV[1]))
 else # must a cron job, looking for @s.
-  tweet.set_triple_array
   reply_ids = []
   tweet.replies.each do |reply|
     reply_ids.push(reply.id)
-    # puts "reply: #{reply.id} - #{reply.text}"
-    match = /^.*@MLArkov:* (\w*).*$/i.match(reply.text)
-    match.nil? ? term = "No term detected" : term = match[1]
-    tweet_text = tweet.build_sentence(term)
-    tweet_text = "@" + reply.user.screen_name + " " + tweet_text
+    puts "reply: #{reply.id} - #{reply.text}"
+    tweet_text = "@" + reply.user.screen_name + " " + tweet.random_sentence
+    puts tweet_text
     tweet.tweet(tweet_text, reply.id)
   end
   # Run this after everything instead and get a better way of getting the most recent since_id
